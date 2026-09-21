8041f4198c07147ea51281f179ff50ff35e35615
Long Vu <longvu@gmail.com>
2026-09-21 16:39:31 +0700

    Remove check script

--- check_ds_len.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-const ds = fs.readFileSync('E:/congty/Code/thuanphat/js/data-store.js', 'utf8');
-const match = ds.match(/const DEFAULT_PRODUCTS = (\[[\s\S]*?\]);/);
-if (match) {
-  const prods = JSON.parse(match[1]);
-  console.log('DEFAULT_PRODUCTS length in data-store.js:', prods.length);
-} else {
-  console.log('Could not parse DEFAULT_PRODUCTS');
-}
